a8b74d3c4b33ea39091977bde08bf02a7671ff81
blakcupi <94331682+blakcupi@users.noreply.github.com>
2024-07-09 23:31:35 +0900

    GetWindowHandle 함수 추가

--- CSWrapper.cs
+++ CSWrapper.cs
@@ -13,16 +13,35 @@ namespace KimCGLib
     {
         //사용할 API함수를 임포트 한다.
         [DllImport("USER32.DLL")]
-        public static extern uint FindWindow(string lpClassName, string lpWindowName);
+        public static extern uint FindWindow(string lpClassName, string lpWindowName); // 주어진 윈도우의 핸들 조회(null, "윈도우 창 제목")
 
         [DllImport("user32.dll")]
-        public static extern uint FindWindowEx(uint hWnd1, uint hWnd2, string lpsz1, string lpsz2);
+        public static extern uint FindWindowEx(uint hWnd1, uint hWnd2, string lpsz1, string lpsz2); // 주어진 윈도우의 하위 객체(버튼, 텍스트, 라디오, ...) 핸들 조회(핸들1, 핸들2, "객체명", "객체캡션")
 
         [DllImport("user32.dll")]
-        public static extern uint SendMessage(uint hwnd, uint wMsg, uint wParam, uint lParam);
+        public static extern uint SendMessage(uint hwnd, uint wMsg, uint wParam, uint lParam); // 동기식 메세지(이벤트) 전송(핸들, 메세지, 0, 0)
 
         [DllImport("user32.dll")]
-        public static extern uint PostMessage(uint hwnd, uint wMsg, uint wParam, uint lParam);
+        public static extern uint PostMessage(uint hwnd, uint wMsg, uint wParam, uint lParam); // 비동기식 메세지(이벤트) 전송(핸들, 메세지, 0, 0)
+
+        [DllImport("user32")]
+        public static extern bool SetForegroundWindow(IntPtr handle); // 윈도우 앞으로 올리기
+
+        // 윈도우 핸들 얻기...2024.07.09
+        public IntPtr GetWindowHandle(string pProcessName)
+        {
+            IntPtr handle = null;
+            foreach(Process process in Process.GetProcesses())
+            {
+            	if(process.ProcessName.StartsWith(pProcessName))
+                {
+                    handle = process.MainWindowHandle;
+                    break;
+                }
+            }
+
+            return handle;
+        }
     }
 }
 
